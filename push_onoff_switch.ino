3e847f9c1d9ee01cbf9e20c3d351e77c26b03e1a
Haowen-Deng <hd2612@NYU.EDU>
2024-11-13 06:52:45 +0800

    Initial commit

---
+++ push_onoff_switch.ino
@@ -0,0 +1,35 @@
+int ledPin = 2;
+int buttonPin = 25;
+int brightPotPin = A0;
+int brightPotValue = 0;
+int brightnessLed = 0;
+
+bool buttonState = LOW;
+bool lastButtonState = LOW;
+bool buttonOn = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  brightPotValue = analogRead(A0);
+  brightnessLed = map(brightPotValue, 0, 1023, 0, 255);
+  lastButtonState = buttonState;
+  buttonState = digitalRead(buttonPin);
+
+  if (lastButtonState == LOW and buttonState == HIGH) {
+    buttonOn = !buttonOn;
+    delay(5);
+  } else if (lastButtonState == HIGH and buttonState == LOW) {
+    delay(5);
+  }
+
+  if (buttonOn == true) {
+    analogWrite(ledPin, brightnessLed);
+  }
+  else {
+    analogWrite(ledPin, 0);
+  }
+}
